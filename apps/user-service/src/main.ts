0e04a448f6439f9bc8689a08ad719ba764839a8a
Anuragreddy Vitta <avitta@berkadia.com>
2021-07-03 18:49:16 +0530

    updated port

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = 8080;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
